606290d5a18b34d390bda021dc57895cd82a10c8
Mabakalari <86472563+Mabakalari@users.noreply.github.com>
2022-08-25 21:52:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.user.setPresence({ activities: [{ name: `${prefix}help`}], status: 'onlin
 client.on('guildCreate', guild => {
   if (guild.id !== sv) return guild.leave()
 })
-client.login("MTAxMjM0MDI0NjgxMTI1NDc4NA.G5YM67.Sb2FKaiqQj5LV5WDUlHP_PKMWRnciFAcQk4LlY");
+client.login("MTAxMjQxNDQ2NDk3ODU5NTg5MQ.GMG_k9.Elbn7GOK7Ag_fqQAdFIbJpxaneGNdaVwUjL90c");
 
 client.on("message", async message => {
 
